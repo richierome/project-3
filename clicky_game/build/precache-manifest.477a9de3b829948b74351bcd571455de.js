00414e25a66b3adfd8ea6f77734158c7d7aa7a47
richie <rickromero99@gmail.com>
2019-06-19 20:42:44 -0700

    updated

---
+++ clicky_game/build/precache-manifest.477a9de3b829948b74351bcd571455de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ace58068de280f581726581035be480",
+    "url": "/richierome/Clicky-Game/index.html"
+  },
+  {
+    "revision": "4006ec934316dabea3ea",
+    "url": "/richierome/Clicky-Game/static/css/main.fcf423bb.chunk.css"
+  },
+  {
+    "revision": "21e0455ab157e3c726f6",
+    "url": "/richierome/Clicky-Game/static/js/2.4282bdd0.chunk.js"
+  },
+  {
+    "revision": "4006ec934316dabea3ea",
+    "url": "/richierome/Clicky-Game/static/js/main.210aaf03.chunk.js"
+  },
+  {
+    "revision": "ac1c078f8fa390c9bd7d",
+    "url": "/richierome/Clicky-Game/static/js/runtime~main.b5f23e36.js"
+  }
+]);
--- clicky_game/src/components/navabar/Navbar.js
+++ clicky_game/src/components/navabar/Navbar.js
@@ -5,7 +5,6 @@ const Navbar = props => (
     
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark"> 
     <h1 className="navbar-text">Richie's Fighter </h1>
-  
     <span className="navbar-text card-guess">{props.cardGuess}</span>
     <span className="navbar-text current-score">Current Score: {props.currentScore}</span>
     <span className="navbar-text high-score">High Score: {props.highScore}</span>
